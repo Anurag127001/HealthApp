f5602f8655b3462d7be4af9f25633a8f7190fb05
Yash Wagh <yash.wagh_comp22@pccoer.in>
2024-01-30 21:39:41 +0530

    Initial commit

---
+++ lib/griddoctor.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class Griddoctor extends StatelessWidget {
+  Griddoctor({Key? key});
+
+  List img = ['Add Patients', 'View Patients', ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: EdgeInsets.only(top: 55),
+      //physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      children: [
+
+        for (int i = 0; i < img.length; i++)
+          Container(
+            child: Card(
+              elevation: 5,
+              child: Column(
+                children: [
+
+                  InkWell(
+                    onTap: () {
+                      print("hello yash");
+                    },
+
+                    child: Image.asset(
+                      'assets/images/${img[i]}.png',
+                      fit: BoxFit.contain,
+                    ),
+
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('${img[i]}',style: GoogleFonts.pacifico(),)
+                ],
+              ),
+            ),
+          ),
+
+        Container(
+
+          child: Card(
+            elevation: 5,
+            child: Column(
+              children: [
+
+                InkWell(
+                  onTap: () {
+                    print("hello yash");
+                  },
+
+                  child: Image.asset(
+                    'assets/images/Data Analytics.png',
+                    fit: BoxFit.contain,
+                  ),
+
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text('Data Analytics',style: GoogleFonts.pacifico(),)
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+
+  }
+}
